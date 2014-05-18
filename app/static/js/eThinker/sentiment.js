b212f8d44e12f38a6e9ed156ff254c461c39733a
echaroy <echaroy@gmail.com>
2014-05-18 09:12:49 +0200

    SentimentGraph Changes

--- app/static/js/eThinker/sentiment.js
+++ app/static/js/eThinker/sentiment.js
@@ -1,77 +1,142 @@
 function sentiment(a,b){
-  var chart = c3.generate({
-         size: {
-                 height: b,
-                 width: a
-             },
-    
-    data: {
-             x: 'x',
-              columns: [
-                  ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06', '2013-01-07', '2013-01-08', '2013-01-09', '2013-01-10'],
-                  ['SentimientoPositivo', 4, 3, 3, 4, 6, 6,7.5,7,9,10],
-                  ['SentimientoNegativo',10, 9, 7, 7, 4, 5,2,3,2,1],
-                  ['SentimientoNeutro',4,5,1,7,8,7,6,2,4,8],
-                  /*['Menciones',10343,11323,23355,22211,53211,43211,42322,55754,12222,12223]*/
-              ],
-              type: 'spline',
-              axes: {
-                          SentimientoPositivo: 'y',
-                          SentimientoNegativo: 'y',
-                          SentimientoNeutro: 'y',
-                          /*Menciones: 'y2',*/
-                      },
-    },
-    axis: {
-        x: {
-            type: 'timeseries',
-            label: 'Periodo',
-            position: 'outer-center',
-            tick: {
-                format: '%Y-%m-%d'
-            }
-        },
-        y: {
-                    max: 10,
-                    min: 0,
-                    label: {
-                           text: 'Sentimiento',
-                           position: 'outer-middle'
-                    }
-             },
-        y2: {
-                    show: false,
-                    position: 'outer-middle',
-                    label: {
-                           text: 'Menciones',
-                           position: 'outer-middle',
-                    }
-                }
-    },
-    zoom: {
-      enabled: true
-    },
-    subchart: {
-            show: true
-    },
-    grid: {
-            x: {
-                lines: [{value: '2013-01-05', text: 'Barcenas Imputado'}]
-            }
-        }
-  });
-  
-  setTimeout(function () {
-      chart.toAreaSpline('SentimientoNegativo');
-  }, 500);
-  setTimeout(function () {
-      chart.toAreaSpline('SentimientoPositivo');
-  }, 1000);
-  setTimeout(function () {
-      chart.toAreaSpline('SentimientoNeutro');
-  }, 1500);
+  var margin = {top: 10, right: 80, bottom: 100, left: 40},
+      margin2 = {top: 430, right: 80, bottom: 50, left: 0},
+      width = a - margin.left - margin.right,
+      height = b - margin.top - margin.bottom,
+      height2 = b - margin2.top - margin2.bottom;;
 
-  /*setTimeout(function () {
-      chart.toAreaSpline('Menciones');
-  }, 6000);*/
+var parseDate = d3.time.format("%Y-%m-%d").parse;
+
+var x  = d3.time.scale().range([0, width]),
+    x2 = d3.time.scale().range([0, width]);
+
+var y = y = d3.scale.linear().range([height, 0]),
+    y2 = d3.scale.linear().range([height2, 0]);
+
+var xAxis  = d3.svg.axis().scale(x).orient("bottom")
+    xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
+    yAxis  = d3.svg.axis().scale(y).orient("left");
+
+var brush = d3.svg.brush().x(x2).on("brush", brushed);
+
+var color = d3.scale.category10();
+
+var line = d3.svg.line()
+    .interpolate("basis")
+    .x(function(d) {return x(d.date); })
+    .y(function(d) { return y(d.sentiment); });
+
+var line2 = d3.svg.line()
+    .interpolate("monotone")
+    .x(function(d) { return x2(d.date); })
+    .y(function(d) { return y2(d.mentions); });
+
+var svg = d3.select(".information").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("class", "focus")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+var context = svg.append("g")
+    .attr("class", "context")
+    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+d3.json("/static/data/sentiment.json", function(error, data) {
+  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "dateSentiment"; }));
+
+  data.sentiments.forEach(function(d) {
+    d.dateSentiment = parseDate(d.dateSentiment);
+  });
+
+  var positiveSentiment = ({name: "Positive",values: []});
+  var negativeSentiment = ({name: "Negative",values: []});
+  var neutralSentiment = ({name: "Neutral",values: []});
+  var mentions = ({name: "Mentions",values: []});
+  var dateDomain = [];
+
+  data.sentiments.forEach(function(d){
+    positiveSentiment.values.push({"date": d.dateSentiment, "sentiment": +d.positiveSentiment});
+    negativeSentiment.values.push({"date": d.dateSentiment, "sentiment": +d.negativeSentiment});
+    neutralSentiment.values.push({"date": d.dateSentiment, "sentiment": +d.neutralSentiment});
+    mentions.values.push({"date": d.dateSentiment, "mentions": +d.totalMentions});
+    dateDomain.push(d.dateSentiment);
+    
+  });
+  sentiments = [positiveSentiment,negativeSentiment];
+
+  x.domain([d3.min(dateDomain),d3.max(dateDomain)]);
+  
+  y.domain([
+    d3.min(sentiments, function(c) { return d3.min(c.values, function(v) { return v.sentiment; }); }),
+    d3.max(sentiments, function(c) { return d3.max(c.values, function(v) { return v.sentiment; }); })
+  ]);
+
+  x2.domain(x.domain());
+  y2.domain([
+    d3.min(mentions, function(c) { return d3.min(c.values, function(v) { return v.mentions; }); }),
+    d3.max(mentions, function(c) { return d3.max(c.values, function(v) { return v.mentions; }); })
+  ]);
+
+  /*IMPRESIÓN DE LOS EJES*/
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+  context.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height2 + ")")
+      .call(xAxis2);
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("SENTIMENT ANALYSIS");
+
+  /*IMPRESION DE SENTIMIENTOS*/
+  var city = svg.selectAll(".sentiments")
+      .data(sentiments)
+      .enter().append("g")
+      .attr("class", "sentiments");
+  
+  city.append("path")
+      .attr("class", "line")
+      .attr("d", function(d) { 
+        return line(d.values); 
+      })
+      .style("stroke", function(d) { return color(d.name); });
+
+  city.append("text")
+      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+      .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.sentiment) + ")"; })
+      .attr("x", 3)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.name; });
+
+  /*IMPRESION DE MENCIONES*/
+  context.append("path")
+      .data(mentions)
+      .attr("class", "area")
+      .attr("d", function(d) { 
+        return line2(d.values); 
+      });
+
+  context.append("g")
+      .attr("class", "x brush")
+      .call(brush)
+      .selectAll("rect")
+      .attr("y", -6)
+      .attr("height", height2 + 7);
+});
+
+function brushed() {
+  x.domain(brush.empty() ? x2.domain() : brush.extent());
+  focus.select(".line").attr("d", line);
+  focus.select(".x.axis").call(xAxis);
+}
 }
